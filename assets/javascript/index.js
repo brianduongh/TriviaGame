407a6fd64877a79dca43636016d6c53bcf922913
Brian Duong <brianduongh@gmail.com>
2018-11-05 20:44:54 -0600

    functionality finished

---
+++ assets/javascript/index.js
@@ -0,0 +1,155 @@
+// Declaring variables
+    var questions = [
+                      "What is capitol of Indonesia?",
+                      "Where is Jeju Island?",
+                      "How many states are there",
+                      "What tea is orange"
+                    ];
+    var answers = [
+                    ["Jakarta", "Bali", "Java", "Bandung"],
+                    ["Mexico", "Phillipines", "Japan", "South Korea"],
+                    ["52", "50", "51", "49"],
+                    ["Black", "Green", "Thai", "Ice"]
+                  ];
+
+    var correctAnswers = ["Jakarta", "South Korea", "50", "Thai"];
+    var intervalId;
+    var clockRunning = false;
+    var incorrect = 0;
+    var correct = 0;
+    var unaswered = 0;
+
+    // Document ready
+    $(document).ready(function() {
+      // User click start button and starts game
+      $("#startBtn").on("click", function() {
+        // Hide start button and starts game
+        $("#startBtn").hide();
+        startGame();
+      });
+
+      // Initialize game with first question and timer
+      function startGame() {
+        generateQuestion();
+      };
+
+      // Generates a random question
+      function generateQuestion() {
+        var questionIndex = Math.floor(Math.random() * questions.length);
+        // If there are questions in question array, print out the question
+        if (typeof questions !== 'undefined' && questions.length > 0) {
+          $("#question").text(questions.splice(questionIndex,1));
+          generateAnswerChoices(questionIndex);
+          timer(questionIndex);
+        } else {
+          showScore();
+        }
+      }
+
+      // Creates timer that passes correct answer down
+      function timer(correctIndex) {
+        var time = 5;
+        $("#timer").text(time);
+        intervalId = setInterval(count, 1000);
+
+        // Resets the time
+        function reset() {
+          time = 5;
+        }
+
+        // Clears time
+        function stop() {
+          console.log("stopping");
+          clearInterval(intervalId);
+        }
+
+        function count() {
+          time--;
+          $("#timer").text(time);
+          // If user runs out of time
+          if (time == 0) {
+            unaswered++;
+            stop();
+            reset();
+            clearQA();
+            showAnswer(correctIndex);
+          }
+        }
+      }
+
+      // Generates answer choices based on question generated
+      function generateAnswerChoices(answersIndex) {
+        // For each answer, make an answer choice
+        answers[answersIndex].forEach(function(answer) {
+          var answerChoice = $("<div>");
+          answerChoice.addClass("answer");
+          answerChoice.attr("id", answer);
+          answerChoice.text(answer);
+          // Add answers to answerChoice div
+          $("#answerChoices").prepend(answerChoice);
+        });
+        // Removes answers from answers array
+        answers.splice(answersIndex, 1);
+
+        // User clicks on answer choice
+        $(".answer").on("click", function() {
+          // If answer is in the answer array
+          if (correctAnswers.indexOf(this.id) > -1) {
+            correct++;
+            clearQA();
+            clearInterval(intervalId);
+            showAnswer(answersIndex, correct);
+          }
+          // If answer is not in answer array
+          else {
+            incorrect++;
+            clearQA();
+            clearInterval(intervalId);
+            showAnswer(answersIndex, incorrect);
+          }
+        });
+      }
+
+      // Shows answer
+      function showAnswer(correctIndex, rightWrong) {
+        // If user is correct
+        if (rightWrong == correct) {
+          $("#result").text("Correct the answer is " + correctAnswers[correctIndex]);
+        }
+        // If user is incorrect
+        else if (rightWrong == incorrect) {
+          $("#result").text("Incorrect the answer was " + correctAnswers[correctIndex]);
+        }
+        // If user runs out of time
+        else {
+          $("#result").text("Time's up! The answer was " + correctAnswers[correctIndex]);
+        }
+
+        // Removes correct answer from array
+        correctAnswers.splice(correctIndex, 1);
+        // After X amount of seconds, move onto next question
+        setTimeout(function() {
+          clearQA();
+          generateQuestion();
+        }, 3000)
+      }
+
+      // Shows score
+      function showScore() {
+        clearQA();
+        $("#result").append("Your score");
+        $("#result").append("<div>Correct answers: " + correct + "</div>")
+        $("#result").append("<div>Incorrect answers: " + incorrect + "</div>");
+        $("#result").append("<div>Unaswered answers: " + unaswered + "</div>");
+      }
+
+
+      // Clears divs
+      function clearQA() {
+        $("#answerChoices").empty();
+        $("#question").empty();
+        $("#result").empty();
+        $("#timer").empty();
+      }
+
+    });
